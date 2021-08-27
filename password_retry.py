0ff03594924024bd5e63bda98024e2fc0837bc42
Ryan <r803425@gmail.com>
2021-08-27 17:47:29 +0800

    change to while count > 0

--- password_retry.py
+++ password_retry.py
@@ -1,16 +1,15 @@
 password = 'a123456'
-pwd = input('請輸入密碼: ')
-count = 3
+count = 3 #剩餘機會
 out_of_limit = False
 
-while True:
-    count = count - 1
+while count > 0: #要重複做，所以使用while loop
+    pwd = input('請輸入密碼: ') #要寫在這裡，重複要做的事情
     if pwd == password:
         print('登入成功')
-        break
-    elif count == 0:
-        print('你沒機會了，傻B')
-        break
+        break #登入成功，馬上逃出迴圈
     elif pwd != password and not out_of_limit:
+        count = count - 1
         print('密碼錯誤!還有', count, '次機會')
-        pwd = input('請輸入密碼: ')
+        if count == 0:
+            print('你沒機會了，傻B')
+            #break #沒機會了，馬上逃出迴圈
