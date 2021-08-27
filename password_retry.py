7ea4ac8ad4ad2e2055f472958c4d6243e3aced8e
Ryan <r803425@gmail.com>
2021-08-27 17:57:28 +0800

    final version

--- password_retry.py
+++ password_retry.py
@@ -3,13 +3,15 @@ count = 3 #剩餘機會
 out_of_limit = False
 
 while count > 0: #要重複做，所以使用while loop
+    count = count - 1
     pwd = input('請輸入密碼: ') #要寫在這裡，重複要做的事情
     if pwd == password:
         print('登入成功')
         break #登入成功，馬上逃出迴圈
-    elif pwd != password and not out_of_limit:
-        count = count - 1
-        print('密碼錯誤!還有', count, '次機會')
-        if count == 0:
+    else: # pwd != password and not out_of_limit:
+        print('密碼錯誤!')
+        if count > 0:
+            print('還有', count, '次機會')
+        else:
             print('你沒機會了，傻B')
             #break #沒機會了，馬上逃出迴圈
